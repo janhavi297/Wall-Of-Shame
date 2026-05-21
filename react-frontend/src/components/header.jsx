94938b2dc594514bb9197e56eaa129a0dbde7c05
janhavi297 <janhavich99@gmail.com>
2026-05-21 18:56:17 +0530

    working on frontend

---
+++ react-frontend/src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div class="text-4xl font-bold underline text text-center">
+        WALL OF SHAME
+    </div>
+  )
+}
